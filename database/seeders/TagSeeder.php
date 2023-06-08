83ca5257a97c5e6b39db82f8b749a68be30db158
Gaetano26 <gaetano.spampinato@outlook.it>
2023-06-08 14:36:04 +0200

    tag seeder and migration complete

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Tag;
+use Illuminate\Support\Str;
 
 class TagSeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = ['layout', 'front-end', 'back-end', 'coding', 'back-office','front-office'];
+        foreach ($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag, '-');
+            $newTag->save();
+
+        }
     }
 }
